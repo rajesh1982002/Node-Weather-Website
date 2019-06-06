4d7afd16315e6c98e12f102fc8787fb90ade2dc0
Rajesh Dhakal <rajesh.dhakal1986@gmail.com>
2019-06-06 16:54:43 -0500

    extra info on forecase windgust

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,16 @@
 const request = require('request')
 
+
+
+
+
+
+
+
+
+
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/748431284b56647743a5050e4952547f/' + latitude + ',' + longitude
 
@@ -9,7 +20,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.'+ ' The nearest storm distance is ' + body.currently.nearestStormDistance  + ' miles and the windgust is about ' + body.currently.windGust + ' mph' )
         }
     })
 }
